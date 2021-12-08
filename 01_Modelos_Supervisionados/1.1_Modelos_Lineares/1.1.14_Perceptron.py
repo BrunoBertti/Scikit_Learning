901095d24ac1dd7027ef695973503873f3dc23f0
BrunoBertti <brunoberttideoliveira@gmail.com>
2021-12-08 10:30:43 -0300

    Update 14

---
+++ 01_Modelos_Supervisionados/1.1_Modelos_Lineares/1.1.14_Perceptron.py
@@ -0,0 +1,11 @@
+########## 1.1.14 Perceptron ##########
+
+    # O Perceptron é outro algoritmo de classificação simples adequado para aprendizagem em larga escala. Por padrão:
+
+        # Não requer taxa de aprendizado.
+
+        # Não é regularizado (penalizado).
+
+        # Atualiza seu modelo apenas em caso de erros.
+
+    # A última característica implica que o Perceptron é um pouco mais rápido de treinar do que o SGD com a perda de articulação e que os modelos resultantes são mais esparsos.
