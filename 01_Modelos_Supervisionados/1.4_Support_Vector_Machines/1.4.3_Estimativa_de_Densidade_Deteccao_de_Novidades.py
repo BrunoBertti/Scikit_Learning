b7891873319821fb8f1601bbfca836b1e8ae77d2
BrunoBertti <brunoberttideoliveira@gmail.com>
2021-12-09 11:19:16 -0300

    Update 04

---
+++ 01_Modelos_Supervisionados/1.4_Support_Vector_Machines/1.4.3_Estimativa_de_Densidade_Deteccao_de_Novidades.py
@@ -0,0 +1,5 @@
+########## 1.4.3 Estimativa de Densidade e Detecção de Novidades ##########
+
+    # A classe OneClassSVM implementa um SVM de uma classe que é usado na detecção de outliers.
+
+    # Veja Novidade e Detecção de Outlier para a descrição e uso de OneClassSVM. (https://scikit-learn.org/stable/modules/outlier_detection.html#outlier-detection)
