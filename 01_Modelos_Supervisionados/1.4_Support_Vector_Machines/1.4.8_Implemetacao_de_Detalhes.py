0ec6d874bba4f5d778353d61db8e5a5f4f1954c8
BrunoBertti <brunoberttideoliveira@gmail.com>
2021-12-10 10:49:56 -0300

    Update 09

---
+++ 01_Modelos_Supervisionados/1.4_Support_Vector_Machines/1.4.8_Implemetacao_de_Detalhes.py
@@ -0,0 +1,21 @@
+########## 1.4.8 Implementação de Detalhes ##########
+
+    # Internamente, usamos libsvm 12 e liblinear 11 para lidar com todos os cálculos. Essas bibliotecas são agrupadas usando C e Cython. Para uma descrição da implementação e detalhes dos algoritmos usados, consulte seus respectivos artigos. 
+
+    ## Referências:
+
+    ## https://www.cs.colorado.edu/~mozer/Teaching/syllabi/6622/papers/Platt1999.pdf
+
+    ## https://www.csie.ntu.edu.tw/~cjlin/papers/svmprob/svmprob.pdf
+
+    ## https://www.csie.ntu.edu.tw/~cjlin/papers/liblinear.pdf <-- Esse
+
+    ## https://www.csie.ntu.edu.tw/~cjlin/papers/libsvm.pdf <-- Esse
+
+    ## https://www.microsoft.com/en-us/research/uploads/prod/2006/01/Bishop-Pattern-Recognition-and-Machine-Learning-2006.pdf
+
+    ## http://citeseerx.ist.psu.edu/viewdoc/summary?doi=10.1.1.114.4288
+
+    ## https://www.stat.purdue.edu/~yuzhu/stat598m3/Papers/NewSVM.pdf
+
+    ## http://jmlr.csail.mit.edu/papers/volume2/crammer01a/crammer01a.pdf
