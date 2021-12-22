692db06f281c0848c93c4414ef7966f3050ff227
BrunoBertti <brunoberttideoliveira@gmail.com>
2021-12-22 14:35:12 -0300

    Update 01

---
+++ 01_Modelos_Supervisionados/1.13_Selecao_de_Variaveis/1.13_Selecao_de_Vairiaveis.py
@@ -0,0 +1,3 @@
+########## 1.13 Seleção de Variáveis ##########
+
+    # As classes no módulo sklearn.feature_selection podem ser usadas para seleção de recursos / redução de dimensionalidade em conjuntos de amostra, seja para melhorar as pontuações de precisão dos estimadores ou para impulsionar seu desempenho em conjuntos de dados dimensionais muito elevados. 
