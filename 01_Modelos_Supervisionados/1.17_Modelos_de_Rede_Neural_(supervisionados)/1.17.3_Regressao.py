7f02df6adfa3b371e75666336fdb4d9600ab92de
BrunoBertti <brunoberttideoliveira@gmail.com>
2021-12-25 10:35:00 -0300

    Update 04

---
+++ 01_Modelos_Supervisionados/1.17_Modelos_de_Rede_Neural_(supervisionados)/1.17.3_Regressao.py
@@ -0,0 +1,5 @@
+########## 1.17.3. Regressão ##########
+
+    # A classe MLPRegressor implementa um perceptron multicamadas (MLP) que treina usando retropropagação sem função de ativação na camada de saída, que também pode ser vista como usando a função de identidade como função de ativação. Portanto, ele usa o erro quadrado como a função de perda e a saída é um conjunto de valores contínuos.
+
+    # O MLPRegressor também oferece suporte à regressão de múltiplas saídas, na qual uma amostra pode ter mais de um destino. 
