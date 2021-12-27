211b50d69cf9219673152071939d5951d9a16089
BrunoBertti <brunoberttideoliveira@gmail.com>
2021-12-27 11:16:31 -0300

    Update 05

---
+++ 01_Modelos_Supervisionados/1.17_Modelos_de_Rede_Neural_(supervisionados)/1.17.4_Regularizacao.py
@@ -0,0 +1,14 @@
+########## 1.17.4. Regularização ##########
+
+    # Tanto o MLPRegressor quanto o MLPClassifier usam o parâmetro alfa para o termo de regularização (regularização L2) que ajuda a evitar sobreajuste ao penalizar pesos com grandes magnitudes. O gráfico a seguir exibe funções de decisão variadas com valor de alfa.
+
+        # https://scikit-learn.org/stable/auto_examples/neural_networks/plot_mlp_alpha.html
+
+
+    # Veja os exemplos abaixo para mais informações. 
+
+
+
+    ## Exemplos:
+
+    ## https://scikit-learn.org/stable/auto_examples/neural_networks/plot_mlp_alpha.html#sphx-glr-auto-examples-neural-networks-plot-mlp-alpha-py
