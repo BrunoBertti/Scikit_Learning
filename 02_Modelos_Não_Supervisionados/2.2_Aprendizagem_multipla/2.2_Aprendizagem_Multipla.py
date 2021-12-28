580bdd72a49e28db76b264d6ad341a69b0372978
BrunoBertti <brunoberttideoliveira@gmail.com>
2021-12-28 10:53:21 -0300

    Update 01

---
+++ 02_Modelos_Não_Supervisionados/2.2_Aprendizagem_multipla/2.2_Aprendizagem_Multipla.py
@@ -0,0 +1,14 @@
+########## 2.2. Aprendizagem múltipla ##########
+
+                                                            # Procure as necessidades básicas
+                                                            # As necessidades básicas simples
+                                                            # Esqueça suas preocupações e conflitos
+                                                            # Quero dizer as necessidades básicas
+                                                            # Receitas da velha mãe natureza
+                                                            # Que trazem as necessidades básicas da vida
+
+                                                            #- Canção de Baloo [The Jungle Book
+
+        # https://scikit-learn.org/stable/auto_examples/manifold/plot_compare_methods.html 
+
+    # O aprendizado múltiplo é uma abordagem para a redução da dimensionalidade não linear. Os algoritmos para essa tarefa são baseados na ideia de que a dimensionalidade de muitos conjuntos de dados é apenas artificialmente alta. 
