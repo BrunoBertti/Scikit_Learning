5623734a898d9cf83c13cfb3dc1104a9a198784a
BrunoBertti <brunoberttideoliveira@gmail.com>
2022-01-08 11:44:04 -0300

    Update 01

---
+++ 02_Modelos_Não_Supervisionados/2.8_Estimativa_de_Densidade/2.8_Estimativa_de_Densidade.py
@@ -0,0 +1,5 @@
+########## 2.8. Estimativa de Densidade ##########
+
+    # A estimativa de densidade caminha na linha entre aprendizado não supervisionado, engenharia de recursos e modelagem de dados. Algumas das técnicas de estimativa de densidade mais populares e úteis são modelos de mistura, como Gaussian Mixture (GaussianMixture), e abordagens baseadas em vizinho, como a estimativa de densidade de kernel (KernelDensity). As misturas gaussianas são discutidas mais detalhadamente no contexto de agrupamento, porque a técnica também é útil como um esquema de agrupamento não supervisionado.
+
+    # A estimativa da densidade é um conceito muito simples e a maioria das pessoas já está familiarizada com uma técnica comum de estimativa da densidade: o histograma. 
