a1b4d33d7cf7638719aced4273980f52252ef8ea
BrunoBertti <brunoberttideoliveira@gmail.com>
2022-01-10 11:42:17 -0300

    Update 05

---
+++ 03_Seleção_de_Modelo_e_Avaliação/3.1_Validacao_Cruzada_Avaliando_o_Desempenho_do_Estimador/3.1.4_Validacao_Cruzada_e_Selecao_de_Modelo.py
@@ -0,0 +1,3 @@
+########## 3.1.4. Validação cruzada e seleção de modelo ##########
+
+    # Os iteradores de validação cruzada também podem ser usados para realizar diretamente a seleção do modelo usando o Grid Search para os hiperparâmetros ideais do modelo. Este é o tópico da próxima seção: Ajustando os hiperparâmetros de um estimador. 
