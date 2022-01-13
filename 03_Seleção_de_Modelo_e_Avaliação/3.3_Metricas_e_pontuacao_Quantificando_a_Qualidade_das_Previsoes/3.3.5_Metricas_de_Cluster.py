6a0a29f037629d207db60c7f77542bfbf612407b
BrunoBertti <brunoberttideoliveira@gmail.com>
2022-01-13 11:18:20 -0300

    Update 06

---
+++ 03_Seleção_de_Modelo_e_Avaliação/3.3_Metricas_e_pontuacao_Quantificando_a_Qualidade_das_Previsoes/3.3.5_Metricas_de_Cluster.py
@@ -0,0 +1,4 @@
+##########  3.3.5. Métricas de cluster ##########
+
+
+    # O módulo sklearn.metrics implementa várias funções de perda, pontuação e utilidade. Para obter mais informações, consulte a seção Avaliação de desempenho de clustering para clustering de instância e Avaliação de biclustering para biclustering. 
