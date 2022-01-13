eedbbfa5da9889482623ebaa08348615884e0b09
BrunoBertti <brunoberttideoliveira@gmail.com>
2022-01-13 11:18:42 -0300

    Update 00

---
+++ 03_Seleção_de_Modelo_e_Avaliação/3.3_Metricas_e_pontuacao_Quantificando_a_Qualidade_das_Previsoes/3.3.4_Metricas_de_Regressao.py
@@ -0,0 +1,29 @@
+##########  3.3.4. Métricas de regressão ##########
+
+
+
+##### 3.3.4.1. Pontuação de variação explicada
+
+##### 3.3.4.2. Erro máximo
+
+##### 3.3.4.3. Erro absoluto médio
+
+##### 3.3.4.4. Erro quadrático médio
+
+##### 3.3.4.5. Erro logarítmico quadrado médio
+
+##### 3.3.4.6. Erro percentual absoluto médio
+
+##### 3.3.4.7. Erro absoluto mediano
+
+##### 3.3.4.8. R² score, o coeficiente de determinação
+
+##### 3.3.4.9. Desvios médios de Poisson, Gamma e Tweedie
+
+##### 3.3.4.10. Pontuação D², o coeficiente de determinação
+
+##### 3.3.4.11. Perda de pinball 
+
+
+
+
