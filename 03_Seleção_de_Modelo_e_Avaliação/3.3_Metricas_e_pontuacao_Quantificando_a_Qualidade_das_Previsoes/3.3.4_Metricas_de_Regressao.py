b62968e80113b45b4dcf8cedfb27b6636ac7a1ea
BrunoBertti <brunoberttideoliveira@gmail.com>
2022-01-15 11:38:36 -0300

    Update 05

--- 03_Seleção_de_Modelo_e_Avaliação/3.3_Metricas_e_pontuacao_Quantificando_a_Qualidade_das_Previsoes/3.3.4_Metricas_de_Regressao.py
+++ 03_Seleção_de_Modelo_e_Avaliação/3.3_Metricas_e_pontuacao_Quantificando_a_Qualidade_das_Previsoes/3.3.4_Metricas_de_Regressao.py
@@ -221,11 +221,56 @@ r2_score(y_true, y_pred, multioutput=[0.3, 0.7])
 ##### 3.3.4.9. Desvios médios de Poisson, Gamma e Tweedie
 
 
+    # A função mean_tweedie_deviance calcula o erro médio de desvio de Tweedie com um parâmetro de potência (P). Essa é uma métrica que obtém valores de expectativa previstos de alvos de regressão.
+
+    # Existem casos especiais a seguir,
+
+        # quando power=0 é equivalente a mean_squared_error.
+
+        # quando power=1 é equivalente a mean_poisson_deviance.
+
+        # quando power=2 é equivalente a mean_gamma_deviance.
+
+    # Se \hat{y}_i é o valor previsto da i-ésima amostra, e y_i é o valor verdadeiro correspondente, então o erro médio de desvio de Tweedie (D) para potência p, estimado sobre n_{\text{amostras}} é definido como 
 
 
+        # \begin{split}\text{D}(y, \hat{y}) = \frac{1}{n_\text{samples}}
+        # \sum_{i=0}^{n_\text{samples} - 1}
+        # \begin{cases}
+        # (y_i-\hat{y}_i)^2, & \text{for }p=0\text{ (Normal)}\\
+        # 2(y_i \log(y/\hat{y}_i) + \hat{y}_i - y_i),  & \text{for}p=1\text{ (Poisson)}\\
+        # 2(\log(\hat{y}_i/y_i) + y_i/\hat{y}_i - 1),  & \text{for}p=2\text{ (Gamma)}\\
+        # 2\left(\frac{\max(y_i,0)^{2-p}}{(1-p)(2-p)}-
+        # \frac{y\,\hat{y}^{1-p}_i}{1-p}+\frac{\hat{y}^{2-p}_i}{2-p}\right),
+        # & \text{otherwise}
+        # \end{cases}\end{split}
 
 
+    # O desvio de Tweedie é uma função homogênea do poder de grau 2. Assim, a distribuição Gamma com poder=2 significa que escalar simultaneamente y_true e y_pred não tem efeito sobre o desvio. Para a distribuição Poisson poder=1 a deviance escala linearmente, e para a distribuição Normal (potência=0), quadrática. Em geral, quanto maior a potência, menos peso é dado aos desvios extremos entre os alvos verdadeiros e previstos.
 
+    # Por exemplo, vamos comparar as duas previsões 1.0 e 100 que são 50% de seu valor verdadeiro correspondente.
+
+    # O erro quadrático médio (potência = 0) é muito sensível à diferença de previsão do segundo ponto: 
+
+from sklearn.metrics import mean_tweedie_deviance
+mean_tweedie_deviance([1.0], [1.5], power=0)
+
+mean_tweedie_deviance([100.], [150.], power=0)
+
+    # Se aumentarmos a potência para 1,: 
+
+mean_tweedie_deviance([1.0], [1.5], power=1)
+
+mean_tweedie_deviance([100.], [150.], power=1)
+
+    # a diferença de erros diminui. Finalmente, definindo, power=2: 
+
+mean_tweedie_deviance([1.0], [1.5], power=2)
+
+mean_tweedie_deviance([100.], [150.], power=2)
+
+
+    # obteríamos erros idênticos. O desvio quando power=2 é, portanto, apenas sensível a erros relativos. 
 
 
 
@@ -234,9 +279,19 @@ r2_score(y_true, y_pred, multioutput=[0.3, 0.7])
 ##### 3.3.4.10. Pontuação D², o coeficiente de determinação
 
 
+    # A função d2_tweedie_score calcula a porcentagem de desvio explicada. É uma generalização do R², onde o erro quadrático é substituído pelo desvio de Tweedie. D², também conhecido como índice de razão de verossimilhança de McFadden, é calculado como
 
+        # D^2(y, \hat{y}) = 1 - \frac{\text{D}(y, \hat{y})}{\text{D}(y, \bar{y})} \ ,.
 
+ 
+    # O poder do argumento define o poder do Tweedie como para mean_tweedie_deviance. Observe que para power=0, d2_tweedie_score é igual a r2_score (para alvos únicos).
 
+    # Assim como o R², a melhor pontuação possível é 1,0 e pode ser negativa (porque o modelo pode ser arbitrariamente pior). Um modelo constante que sempre prevê o valor esperado de y, desconsiderando os recursos de entrada, obteria uma pontuação D² de 0,0.
+
+    # Um objeto marcador com uma escolha específica de poder pode ser construído por: 
+
+from sklearn.metrics import d2_tweedie_score, make_scorer
+d2_tweedie_score_15 = make_scorer(d2_tweedie_score, power=1.5)
 
 
 
@@ -247,5 +302,42 @@ r2_score(y_true, y_pred, multioutput=[0.3, 0.7])
 ##### 3.3.4.11. Perda de pinball 
 
 
+    # A função mean_pinball_loss é usada para avaliar o desempenho preditivo de modelos de regressão quantílica. A perda de pinball é equivalente a mean_absolute_error quando o parâmetro de quantil alfa é definido como 0,5. 
+
+        # \text{pinball}(y, \hat{y}) = \frac{1}{n_{\text{samples}}} \sum_{i=0}^{n_{\text{samples}}-1}  \alpha \max(y_i - \hat{y}_i, 0) + (1 - \alpha) \max(\hat{y}_i - y_i, 0)
 
 
+from sklearn.metrics import mean_pinball_loss
+y_true = [1, 2, 3]
+mean_pinball_loss(y_true, [0, 2, 3], alpha=0.1)
+mean_pinball_loss(y_true, [1, 2, 4], alpha=0.1)
+mean_pinball_loss(y_true, [0, 2, 3], alpha=0.9)
+mean_pinball_loss(y_true, [1, 2, 4], alpha=0.9)
+mean_pinball_loss(y_true, y_true, alpha=0.1)
+mean_pinball_loss(y_true, y_true, alpha=0.9)
+
+    # É possível construir um objeto scorer com uma escolha específica de alfa: 
+
+from sklearn.metrics import make_scorer
+mean_pinball_loss_95p = make_scorer(mean_pinball_loss, alpha=0.95)
+
+    # Esse marcador pode ser usado para avaliar o desempenho de generalização de um regressor quantil por meio de validação cruzada: 
+
+from sklearn.datasets import make_regression
+from sklearn.model_selection import cross_val_score
+from sklearn.ensemble import GradientBoostingRegressor
+X, y = make_regression(n_samples=100, random_state=0)
+estimator = GradientBoostingRegressor(
+    loss="quantile",
+    alpha=0.95,
+    random_state=0,
+)
+cross_val_score(estimator, X, y, cv=5, scoring=mean_pinball_loss_95p)
+
+    # Também é possível construir objetos de pontuação para ajuste de hiperparâmetros. O sinal da perda deve ser trocado para garantir que maior significa melhor, conforme explicado no exemplo vinculado abaixo. 
+
+
+
+    ## Exemplos:
+
+    ## See Prediction Intervals for Gradient Boosting Regression for an example of using a the pinball loss to evaluate and tune the hyper-parameters of quantile regression models on data with non-symmetric noise and outliers. (https://scikit-learn.org/stable/auto_examples/ensemble/plot_gradient_boosting_quantile.html#sphx-glr-auto-examples-ensemble-plot-gradient-boosting-quantile-py)
