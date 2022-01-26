5b77888bd2cc27fed17f1f5d3ad8255ea619fa87
BrunoBertti <brunoberttideoliveira@gmail.com>
2022-01-26 12:54:08 -0300

    Update 01

---
+++ 04_Inspecao/4.1_Graficos_de_Dependencia_Parcial_e_Expectativa_Condicional_Individual/4.1_Graficos_de_Dependencia_Parcial_e_Expectativa_Condicional_Individual.py
@@ -0,0 +1,5 @@
+########## 4.1. Gráficos de dependência parcial e expectativa condicional individual ##########
+
+    # Gráficos de dependência parcial (PDP) e gráficos de expectativa condicional individual (ICE) podem ser usados para visualizar e analisar a interação entre a resposta alvo 1 e um conjunto de recursos de entrada de interesse.
+
+    # Tanto os PDPs quanto os ICEs assumem que os recursos de entrada de interesse são independentes dos recursos do complemento, e essa suposição é frequentemente violada na prática. Assim, no caso de características correlacionadas, criaremos pontos de dados absurdos para calcular o PDP/ICE. 
