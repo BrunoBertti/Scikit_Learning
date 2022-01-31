ca839c7c2369a6dacafe61075e2aa78f12557b4b
BrunoBertti <brunoberttideoliveira@gmail.com>
2022-01-31 08:36:39 -0300

    Update 01

---
+++ 06_Transformacoes_do_Conjunto_de_Dados/6.2_Extracao_de_Recursos/6.2_Extracao_de_Recursos.py
@@ -0,0 +1,6 @@
+##########  6.2. Extração de recursos ##########
+
+
+    # O módulo sklearn.feature_extraction pode ser usado para extrair recursos em um formato compatível com algoritmos de aprendizado de máquina de conjuntos de dados que consistem em formatos como texto e imagem.
+
+    # Nota: A extração de recursos é muito diferente da seleção de recursos: a primeira consiste em transformar dados arbitrários, como texto ou imagens, em recursos numéricos utilizáveis para aprendizado de máquina. O último é uma técnica de aprendizado de máquina aplicada a esses recursos. 
