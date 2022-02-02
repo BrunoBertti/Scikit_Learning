4808efd31171e67f52a6b89942b597896f560102
BrunoBertti <brunoberttideoliveira@gmail.com>
2022-02-02 11:05:44 -0300

    Update 07

---
+++ 06_Transformacoes_do_Conjunto_de_Dados/6.3_Dados_de_Pre-Processamento/6.3.6_Imputacao_de_Valores_Ausentes.py
@@ -0,0 +1,6 @@
+########## 6.3.6. Imputação de valores ausentes ##########
+
+
+
+
+    # Ferramentas para imputar valores omissos são discutidas em Imputação de valores omissos. (https://scikit-learn.org/stable/modules/impute.html#impute)
