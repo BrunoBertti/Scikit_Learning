bde43cbf62f1bbd4cce356a164b554c9f9bf6901
BrunoBertti <brunoberttideoliveira@gmail.com>
2022-02-03 10:02:53 -0300

    Update 02

---
+++ 06_Transformacoes_do_Conjunto_de_Dados/6.4_Imputacao_de_Valores_Ausentes/6.4.1_Imputacao_Univariada_vs_Multivariada.py
@@ -0,0 +1,5 @@
+########## 6.4.1. Imputação univariada vs. multivariada ##########
+
+
+
+    # Um tipo de algoritmo de imputação é univariado, que imputa valores na i-ésima dimensão de recurso usando apenas valores não omissos nessa dimensão de recurso (por exemplo, impute.SimpleImputer). Por outro lado, os algoritmos de imputação multivariada usam todo o conjunto de dimensões de recursos disponíveis para estimar os valores ausentes (por exemplo, impute.IterativeImputer). 
