db7437896a770e347e5270e51fffc6e7cb2155b5
BrunoBertti <brunoberttideoliveira@gmail.com>
2022-02-04 08:56:35 -0300

    Update 05

---
+++ 06_Transformacoes_do_Conjunto_de_Dados/6.4_Imputacao_de_Valores_Ausentes/6.4.4_Referencias.py
@@ -0,0 +1,11 @@
+########## 6.4.4. Referências ##########
+
+
+
+
+
+
+
+    ## 1 Stef van Buuren, Karin Groothuis-Oudshoorn (2011). “mice: Multivariate Imputation by Chained Equations in R”. Journal of Statistical Software 45: 1-67.
+
+    ## 2 Roderick J A Little and Donald B Rubin (1986). “Statistical Analysis with Missing Data”. John Wiley & Sons, Inc., New York, NY, USA.
