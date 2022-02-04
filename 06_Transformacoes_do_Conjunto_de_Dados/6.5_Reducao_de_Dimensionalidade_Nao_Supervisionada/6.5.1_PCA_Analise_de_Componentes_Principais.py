2f80cb0ccb14ff000abe0908d150833d6e28e562
BrunoBertti <brunoberttideoliveira@gmail.com>
2022-02-04 08:57:08 -0300

    Update 02

---
+++ 06_Transformacoes_do_Conjunto_de_Dados/6.5_Reducao_de_Dimensionalidade_Nao_Supervisionada/6.5.1_PCA_Analise_de_Componentes_Principais.py
@@ -0,0 +1,9 @@
+########## 6.5.1. PCA: análise de componentes principais ##########
+
+
+    # decomposition.PCA procura uma combinação de recursos que capturem bem a variação dos recursos originais. Consulte Decompondo sinais em componentes (problemas de fatoração de matrizes). 
+
+
+    ## Exemplos:
+
+    ## https://scikit-learn.org/stable/auto_examples/applications/plot_face_recognition.html#sphx-glr-auto-examples-applications-plot-face-recognition-py
