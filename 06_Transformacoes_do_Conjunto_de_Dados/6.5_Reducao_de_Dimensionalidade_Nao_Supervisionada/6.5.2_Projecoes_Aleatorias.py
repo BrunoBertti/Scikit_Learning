d86d93df995b3694765063179d38449259d9a8b2
BrunoBertti <brunoberttideoliveira@gmail.com>
2022-02-05 11:42:38 -0300

    Update 03

---
+++ 06_Transformacoes_do_Conjunto_de_Dados/6.5_Reducao_de_Dimensionalidade_Nao_Supervisionada/6.5.2_Projecoes_Aleatorias.py
@@ -0,0 +1,9 @@
+########## 6.5.2. projeções aleatórias ##########
+
+
+    # O módulo: random_projection disponibiliza diversas ferramentas para redução de dados por projeções aleatórias. Consulte a seção relevante da documentação: Projeção aleatória. 
+
+
+    ## Exemplos:
+
+    ## https://scikit-learn.org/stable/auto_examples/miscellaneous/plot_johnson_lindenstrauss_bound.html#sphx-glr-auto-examples-miscellaneous-plot-johnson-lindenstrauss-bound-py
