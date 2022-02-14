3b35f238244416fd5f3bee27d97a5421d9bac05b
BrunoBertti <brunoberttideoliveira@gmail.com>
2022-02-14 09:42:15 -0300

    Update 01

---
+++ 06_Transformacoes_do_Conjunto_de_Dados/6.9_Transformando_a_Meta_de_Previsao_(y)/6.9_Transformando_a_Meta_de_Previsao_(y).py
@@ -0,0 +1,4 @@
+########## 6.9. Transformando a meta de previsão (y) ##########
+
+
+    # Estes são transformadores que não se destinam a ser usados em recursos, apenas em alvos de aprendizado supervisionado. Consulte também Transformando destino em regressão se desejar transformar o destino de previsão para aprendizado, mas avaliar o modelo no espaço original (não transformado). 
